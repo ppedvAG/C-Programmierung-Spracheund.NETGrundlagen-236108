38dc10dfe532e4a7f774a2032099bf6252782cd3
ppedvLukasK <lukask@ppedv.de>
2024-01-16 11:47:06 +0100

    M001-M003

---
+++ M002/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace M002
+{
+	/// <summary>
+	/// Das ist die Program Klasse
+	/// </summary>
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Variablen
+			//Aufbau:
+			//Datentyp Name = Wert;
+			int zahl = 5;
+			Console.WriteLine(zahl); //cw + Tab
+
+			//Datentypen
+			//Ganze Zahlen:
+			//byte, short, int, long
+			short kurzeZahl;
+
+			//Kommazahlen:
+			//float, double, decimal
+			double d = 239847.321579321;
+			float f = 23598125.3291587193F; //Kommazahlen sind standardmäßig doubles, mit dem F Suffix in einen float konvertieren
+			decimal m = 357923.3215791275M; //Kommazahlen sind standardmäßig doubles, mit dem M Suffix in ein decimal konvertieren
+
+			//Texttypen:
+			//char, string
+			string str = "Das ist ein Text"; //Text unter doppelte Hochkomma setzen
+			char c = 'A'; //Char unter einzelne Hochkomma setzen
+
+			//Boolean
+			bool bt = true;
+			bool bf = false;
+			#endregion
+
+			#region Strings
+			//Strings verbinden
+			//Mit +
+			string kombination = "Die Zahl ist " + zahl + ", der Text ist " + str + ", der Boolean ist " + bt;
+            Console.WriteLine(kombination);
+
+			//String Interpolation ($-String): Ermöglicht, Code in einen String einzubauen
+			string interpolation = $"Die Zahl ist {zahl}, der Text ist {str}, der Boolean ist {bt}";
+            Console.WriteLine(interpolation);
+
+			//Escape-Sequenzen: Untippbare Zeichen in einen String einbaue
+			//https://docs.microsoft.com/en-us/cpp/c-language/escape-sequences?view=msvc-170
+			string umbruch = "Das ist\n ein Text"; //\n: Zeilenumbruch
+            Console.WriteLine(umbruch);
+
+			string weitere = "\t \" \' \\";
+
+			//Verbatim String (@-String): Nimmt den String 1:1 wie er im Code steht
+			string verbatim = @"Das ist
+ein Text\n";
+            Console.WriteLine(verbatim);
+
+			string pfad = @"C:\Program Files\dotnet\shared\Microsoft.NETCore.App\7.0.5\System.Runtime.dll";  //Escape-Sequenzen werden nicht interpretiert
+			#endregion
+
+			#region Eingabe
+			//Konsoleneingabe mit Console.ReadLine(): Liest Text bis Enter gedrückt wird
+			string eingabe = Console.ReadLine(); //Eingabe vom User steht jetzt in der Variable
+			Console.WriteLine($"Du hast {eingabe} eingegeben");
+
+			ConsoleKeyInfo info = Console.ReadKey(); //Lies ein einziges Zeichen vom Benutzer ein (ohne Enter)
+			Console.WriteLine(info.Key);
+			#endregion
+
+			#region Konvertierungen
+			//string -> anderer Typ: Parse
+			//anderer Typ -> string: ToString()
+			//anderer Typ -> anderer Typ: Typecast
+			string parse = "358729";
+			//Console.WriteLine(parse * 2); //Nicht möglich, weil Typen inkompatibel sind
+			int konvertiert = int.Parse(parse); //Ermöglicht, Strings zu einem bestimmten Typen zu konvertieren
+            Console.WriteLine(konvertiert * 2);
+
+			double kd = double.Parse(parse); //String zu einer Kommazahl konvertieren
+            Console.WriteLine(kd);
+
+            //Zu String konvertieren
+            Console.WriteLine("Die Zahl ist " + zahl); //Hier wird implizit konvertiert
+            Console.WriteLine(zahl.ToString()); //Explizite Konvertierung erzwingen
+
+			//Typen ineinander Konvertieren
+			//int z = 32523.9587315; //Nicht implizit kompatibel -> Konvertierung erzwingen
+			int z3 = (int) 32523.9587315; //Konvertierung erzwingen über einen Cast, Typecast, Casting
+            Console.WriteLine(z3); //Kommastellen werden abgeschnitten
+			double d2 = z3; //Hier kein Cast erforderlich, weil alle ints immer in double passen
+
+			long l = 387593217593175981;
+			int i = (int) l; //Hier wird int.MaxValue so oft subtrahiert, bis die resultierende Zahl in int passt
+            Console.WriteLine(i);
+            Console.WriteLine(int.MaxValue); //2^31 - 1, 2147483647
+			#endregion
+
+			#region Arithmetik
+			int z1 = 4;
+			int z2 = 7;
+
+            Console.WriteLine(z1 + z2); //Originale Werte werden nicht verändert, die Summe wird gebildet und ausgegeben
+			z1 += z2; //Addiere z2 auf z1, z1 wird hier verändert
+
+			z1 -= z2;
+			z1 *= z2;
+			z1 /= z2;
+			z1 %= z2; //Schreibe den Rest der Division in die erste Zahl hinein
+
+			z1++;
+			z2--;
+
+			Math.Ceiling(4.5); //Aufrunden
+			Math.Floor(4.5); //Abrunden
+			Math.Round(4.5); //Rundet auf oder ab, .5 wird zur nächsten geraden Zahl gerundet -> 4
+			Math.Round(5.5); //-> 6
+			Math.Round(5.5, 2); //Anzahl Kommastellen angeben
+
+            Console.WriteLine(8 / 5); //1, weil hier eine Int-Division durchgeführt wurde
+            Console.WriteLine(8.0 / 5); //1.6, weil eine der Zahlen eine Kommazahl ist
+            Console.WriteLine(8 / 5.0); //1.6, weil eine der Zahlen eine Kommazahl ist
+            Console.WriteLine(8f / 5); //1.6, weil eine der Zahlen eine Kommazahl ist
+            Console.WriteLine(8d / 5); //1.6, weil eine der Zahlen eine Kommazahl ist
+            Console.WriteLine((double) z1 / z2); //Bei Variablen eine Kommadivision über einen Cast erzwingen
+            #endregion
+        }
+	}
+}
---
+++ M003/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace M003
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Arrays
+			//Array: Variable die mehrere Werte halten kann
+			int[] zahlen;
+			zahlen = new int[5]; //Neues Array erstellen mit Länge 5 (5 Plätze), Index 0-4
+			zahlen[2] = 10; //Bestimmte Stelle angreifen
+			Console.WriteLine(zahlen[2]); //Mittleres Element (Indizes: 0, 1, 2, 3, 4)
+
+			//Direkte Initialisierung
+			int[] zahlen2 = new int[] { 2, 3, 44, 55 }; //Länge 4, Index 0-3
+			int[] zahlen3 = new[] { 2, 3, 44, 55 }; //Länge 4, Index 0-3
+			int[] zahlen4 = { 2, 3, 44, 55 }; //Länge 4, Index 0-3
+			int[] zahlen5 = [2, 3, 44, 55]; //Neu mit C# 12
+
+			Console.WriteLine(zahlen.Length); //Anzahl Plätze (5)
+			Console.WriteLine(zahlen.Contains(10)); //Enthält zahlen 10? -> true
+
+			//Mehrdimensionale Arrays
+			int[,] zweiDArray = new int[3, 3]; //3x3 Matrix
+			zweiDArray[0, 2] = 5; //Oben rechts
+
+			Console.WriteLine(zweiDArray.Rank); //Anzahl der Dimensionen (2)
+			Console.WriteLine(zweiDArray.GetLength(0)); //Länge der ersten Dimension (3)
+			Console.WriteLine(zweiDArray.GetLength(1)); //Länge der zweiten Dimension (3)
+			#endregion
+
+			#region Bedingungen
+			int z1 = 4;
+			int z2 = 7;
+
+			if (z1 == 4 && z2 == 7)
+			{
+                Console.WriteLine(z1 & z2);
+                //100 & 111
+                //100
+                //111
+                //->100
+
+                Console.WriteLine(z1 | z2);
+                //100 | 111
+                //100
+                //111
+                //->111
+            }
+
+			if (z1 == 4 ^ z2 == 7) //XOR
+			{
+				//Nur true, wenn die beiden Bedingungen unterschiedlich sind
+				//Immer false, wenn beide Bedingungen true oder false sind
+			}
+
+			if (!(z1 == 4)) //Negiert eine Bedingung
+			{
+
+			}
+
+			if (!zahlen.Contains(5)) //NotContains existiert nicht -> Contains invertieren mit !
+			{
+
+			}
+
+			//Einzeilige ifs/elses können ohne Klammern geschrieben
+			if (!zahlen.Contains(5))
+                Console.WriteLine("Zahlen enthält nicht 5");
+			else
+				Console.WriteLine("Zahlen enthält 5");
+
+			//if (!zahlen.Contains(5))
+			//	Console.WriteLine("Zahlen enthält nicht 5");
+			//else if (...)
+			//{
+
+			//}
+
+			//Ternary Operator (?-Operator): If/Else Konstrukte vereinfachen
+			if (!zahlen.Contains(5))
+				Console.WriteLine("Zahlen enthält nicht 5");
+			else
+				Console.WriteLine("Zahlen enthält 5");
+
+            Console.WriteLine(zahlen.Contains(5) ? "Zahlen enthält 5" : "Zahlen enthält nicht 5"); //Nach ? kommt der if Teil, nach : kommt der else Teil
+			string str = zahlen.Contains(5) ? "Zahlen enthält 5" : "Zahlen enthält nicht 5";
+			//zahlen.Contains(5) ? Console.WriteLine("Zahlen enthält 5") : Console.WriteLine("Zahlen enthält nicht 5"); //Nicht möglich
+            #endregion
+        }
+	}
+}
