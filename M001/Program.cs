4074ac746af34354bad6f8e2ab914f0fe2b67136
ppedvLukasK <lukask@ppedv.de>
2024-01-19 12:08:57 +0100

    M001

--- M001/Program.cs
+++ M001/Program.cs
@@ -1,7 +1,53 @@
 ﻿public class Program
 {
-	static void Main(string[] args)
+	event Action<int> Prime;
+
+	event Action<int> Prime100;
+
+	event Action<int, int> NoPrime;
+
+    static void Main(string[] args)
+    {
+		Program program = new Program();
+		program.Prime += (i) => Console.WriteLine($"Primzahl: {i}");
+		program.Prime100 += (i) => Console.WriteLine($"Hundertste Primzahl: {i}");
+		program.NoPrime += (i, t) => Console.WriteLine($"Keine Primzahl: {i}, Teiler: {t}");
+		program.DoWork();
+    }
+
+    public void DoWork()
 	{
-		
+		int counter = 0;
+		for (int i = 3; ; i += 2)
+		{
+			if (CheckPrime(i))
+			{
+				counter++;
+				if (counter % 100 == 0 && counter != 0)
+					Prime100?.Invoke(i);
+				else
+					Prime?.Invoke(i);
+			}
+			Thread.Sleep(20);
+		}
+	}
+
+	public bool CheckPrime(int num)
+	{
+		if (num % 2 == 0)
+		{
+			NoPrime?.Invoke(num, 2);
+			return false;
+		}
+
+		for (int i = 3; i <= num / 2; i += 2)
+		{
+			if (num % i == 0)
+			{
+				NoPrime?.Invoke(num, i);
+				return false;
+			}
+		}
+		return true;
 	}
 }
